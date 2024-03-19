3845f3c82b0589cec488701ba5f564c4687f4e60
hamza-25 <me677499@gmail.com>
2024-03-19 22:36:09 +0000

    add min operation

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+""" define factorail module """
+
+
+def minOperations(n: int) -> int:
+    """function that calculate min operation"""
+    divisor: int = 2
+    sum: int = 0
+    while n > 1:
+        while n % divisor == 0:
+            sum += divisor
+            n //= divisor
+        divisor += 1
+    return sum
