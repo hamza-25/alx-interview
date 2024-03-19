590df5b29abf579b51847b4d101262bbf088bbd8
hamza-25 <me677499@gmail.com>
2024-03-19 22:40:23 +0000

    update

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -4,11 +4,17 @@
 
 def minOperations(n: int) -> int:
     """function that calculate min operation"""
+    temp = n
     divisor: int = 2
+    factor = []
     sum: int = 0
     while n > 1:
         while n % divisor == 0:
+            factor.append(divisor)
             sum += divisor
             n //= divisor
         divisor += 1
+    print(n, temp)
+    if len(factor) == 1 and factor[0] == temp:
+        return 0
     return sum
