36003958f463378f43e4aa830f05956cfa79a19e
hamza-25 <me677499@gmail.com>
2024-03-19 22:50:41 +0000

    update

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -2,16 +2,13 @@
 """ define factorail module """
 
 
-def minOperations(n: int) -> int:
+def minOperations(n):
     """function that calculate min operation"""
-    temp = n
-    divisor: int = 2
-    # factor = []
-    sum: int = 0
+    divisor = 2
+    sumOfOperation = 0
     while n > 1:
         while n % divisor == 0:
-            # factor.append(divisor)
-            sum += divisor
-            n //= divisor
+            sumOfOperation += divisor
+            n /= divisor
         divisor += 1
-    return sum
+    return sumOfOperation
