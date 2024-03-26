6b8f878553a928e1d38c8c1ebca2d890fff39b2d
hamza-25 <me677499@gmail.com>
2024-03-26 20:02:03 +0000

    up

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -2,7 +2,6 @@
 """script that reads stdin line by line and computes metrics
 """
 import sys
-import signal
 import re
 
 
@@ -15,22 +14,60 @@ import re
 
 
 # signal.signal(signal.SIGINT, signal_handler)
+# --------------------------------------
+# def print_logs(code_dict, size):
+#     """print format logs"""
+#     print("File size: {}".format(size))
+#     for key, value in code_dict.items():
+#         if value != 0:
+#             print("{}: {}".format(key, value))
+
+
+# size = 0
+# code_dict = {'200': 0, '301': 0, '400': 0, '401': 0,
+#              '403': 0, '404': 0, '405': 0, '500': 0, }
+# i = 0
+
+# try:
+#     for line in sys.stdin:
+
+#         pattern = (
+#             r'^(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}) - '
+#             r'\[(\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}\.\d{1,6})\] '
+#             r'"GET /projects/260 HTTP/1\.1" (\d{3}) (\d{1,10})'
+#         )
+
+#         match = re.search(pattern, line.strip())
+
+#         if match:
+#             status_code = match.group(3)
+#             file_size = match.group(4)
+#             if status_code in code_dict.keys():
+#                 code_dict[status_code] += 1
+#                 size += int(file_size)
+
+#         if i == 9:
+#             print_logs(code_dict, size)
+#             code_dict = {'200': 0, '401': 0, '403': 0,
+#                          '404': 0, '405': 0, '500': 0, }
+#             size = 0
+#             i = -1
+#         i += 1
+# finally:
+#     print_logs(code_dict, size)
 def print_logs(code_dict, size):
-    """print format logs"""
+    """Print format logs."""
     print("File size: {}".format(size))
-    for key, value in code_dict.items():
-        if value != 0:
-            print("{}: {}".format(key, value))
+    for key in sorted(code_dict.keys()):
+        print("{}: {}".format(key, code_dict[key]))
 
 
 size = 0
-code_dict = {'200': 0, '301': 0, '400': 0, '401': 0,
-             '403': 0, '404': 0, '405': 0, '500': 0, }
+code_dict = {}
 i = 0
 
 try:
     for line in sys.stdin:
-
         pattern = (
             r'^(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}) - '
             r'\[(\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}\.\d{1,6})\] '
@@ -42,16 +79,17 @@ try:
         if match:
             status_code = match.group(3)
             file_size = match.group(4)
-            if status_code in code_dict.keys():
+            if status_code.isdigit():
+                code_dict.setdefault(status_code, 0)
                 code_dict[status_code] += 1
                 size += int(file_size)
 
-        if i == 9:
-            print_logs(code_dict, size)
-            code_dict = {'200': 0, '401': 0, '403': 0,
-                         '404': 0, '405': 0, '500': 0, }
-            size = 0
-            i = -1
         i += 1
-finally:
+        if i == 10:
+            print_logs(code_dict, size)
+            code_dict.clear()
+            size = 0
+            i = 0
+
+except KeyboardInterrupt:
     print_logs(code_dict, size)
