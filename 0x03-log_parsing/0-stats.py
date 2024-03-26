828ca74485c041ff96d781bf86a55cbae94c6bd1
hamza-25 <me677499@gmail.com>
2024-03-26 18:29:29 +0000

    up

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -6,42 +6,47 @@ import signal
 import re
 
 
-def signal_handler(sig, frame):
-    """handler crtl + c"""
-    print("File size: {}".format(size))
-    for key, value in code_dict.items():
-        print("{}: {}".format(key, value))
-    sys.exit(0)
+# def signal_handler(sig, frame):
+#     """handler crtl + c"""
+#     print("File size: {}".format(size))
+#     for key, value in code_dict.items():
+#         print("{}: {}".format(key, value))
+#     sys.exit(0)
 
 
-signal.signal(signal.SIGINT, signal_handler)
+# signal.signal(signal.SIGINT, signal_handler)
 
 size = 0
 code_dict = {'200': 0, '401': 0, '403': 0, '404': 0, '405': 0, '500': 0, }
 i = 0
-for line in sys.stdin:
+try:
+    for line in sys.stdin:
 
-    pattern = (
-        r'^(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}) - '
-        r'\[(\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}\.\d{1,8})\] '
-        r'"GET /projects/260 HTTP/1\.1" (\d{3}) (\d{1,10})'
-    )
+        pattern = (
+            r'^(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}) - '
+            r'\[(\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}\.\d{1,8})\] '
+            r'"GET /projects/260 HTTP/1\.1" (\d{3}) (\d{1,10})'
+        )
 
-    match = re.search(pattern, line.strip())
+        match = re.search(pattern, line.strip())
 
-    if match:
-        status_code = match.group(3)
-        file_size = match.group(4)
-        if status_code in code_dict.keys():
-            code_dict[status_code] += 1
-        size += int(file_size)
+        if match:
+            status_code = match.group(3)
+            file_size = match.group(4)
+            if status_code in code_dict.keys():
+                code_dict[status_code] += 1
+            size += int(file_size)
 
-    if i == 9:
-        print("File size: {}".format(size))
-        for key, value in code_dict.items():
-            print("{}: {}".format(key, value))
-        code_dict = {'200': 0, '401': 0, '403': 0,
-                     '404': 0, '405': 0, '500': 0, }
-        size = 0
-        i = -1
-    i += 1
+        if i == 9:
+            print("File size: {}".format(size))
+            for key, value in code_dict.items():
+                print("{}: {}".format(key, value))
+            code_dict = {'200': 0, '401': 0, '403': 0,
+                         '404': 0, '405': 0, '500': 0, }
+            size = 0
+            i = -1
+        i += 1
+finally:
+    print("File size: {}".format(size))
+    for key, value in code_dict.items():
+        print("{}: {}".format(key, value))
