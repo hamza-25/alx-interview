4856f6942b285cf92665a6a78905af1e674935cb
hamza-25 <me677499@gmail.com>
2024-03-26 18:18:14 +0000

    add tasks logs print

---
+++ 0x03-log_parsing/0-stats.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python3
+"""
+"""
+import sys
+import signal
+import re
+
+"""
+
+15.125.5.12 - [2024-03-26 15:09:28.624207] "GET /projects/260 HTTP/1.1" 200 15
+"""
+
+
+def signal_handler(sig, frame):
+    print("File size: {}".format(size))
+    for key, value in code_dict.items():
+        print("{}: {}".format(key, value))
+    sys.exit(0)
+
+
+signal.signal(signal.SIGINT, signal_handler)
+
+size = 0
+code_dict = {'200': 0, '401': 0, '403': 0, '404': 0, '405': 0, '500': 0, }
+i = 0
+for line in sys.stdin:
+
+    # group1 = '(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}) - '
+    # group2 = '\[(\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}\.\d{1,8})\]'
+    # group3 = ' "GET /projects/260 HTTP/1\.1"'
+    # group4 = ' (\d{3}) (\d{1,10})'
+    # conc_group = group1 + group2 + group3 + group4
+    # pattern = r'^' + conc_group
+    # print(pattern)
+    pattern = r'^(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}) - \[(\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}\.\d{1,8})\] "GET /projects/260 HTTP/1\.1" (\d{3}) (\d{1,10})'
+    match = re.search(pattern, line.strip())
+
+    if match:
+        status_code = match.group(3)
+        file_size = match.group(4)
+        if status_code in code_dict.keys():
+            code_dict[status_code] += 1
+        size += int(file_size)
+
+    if i == 9:
+        print("File size: {}".format(size))
+        for key, value in code_dict.items():
+            print("{}: {}".format(key, value))
+        code_dict = {'200': 0, '401': 0, '403': 0,
+                     '404': 0, '405': 0, '500': 0, }
+        size = 0
+        i = -1
+
+    # if line.strip() == 'exit':
+    #     break
+    i += 1
