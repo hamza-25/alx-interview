a113b09b2c23d60d934dda4fedc2c13aba1210df
hamza-25 <me677499@gmail.com>
2024-04-03 23:56:27 +0000

    use bitiwse

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -6,7 +6,28 @@
 def validUTF8(data):
     """validate the ASCCI code
     """
-    for num in data:
-        if num not in range(0, 127):
-            return False
-    return True
+    # for num in data:
+    #     if num not in range(0, 127):
+    #         return False
+    # return True
+    num_bytes = 0
+
+    for byte in data:
+        byte = byte & 0xFF  # Ensure byte is in range 0-255
+        if num_bytes == 0:
+            if byte >> 7 == 0:
+                continue
+            elif byte >> 5 == 0b110:
+                num_bytes = 1
+            elif byte >> 4 == 0b1110:
+                num_bytes = 2
+            elif byte >> 3 == 0b11110:
+                num_bytes = 3
+            else:
+                return False
+        else:
+            if byte >> 6 != 0b10:
+                return False
+            num_bytes -= 1
+
+    return num_bytes == 0
