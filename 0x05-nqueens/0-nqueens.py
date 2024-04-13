ae4a0c2407c1fb3c6a88802be5a76da10e8c2674
hamza-25 <me677499@gmail.com>
2024-04-13 20:58:35 +0000

    add files

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,82 +1,62 @@
 #!/usr/bin/python3
 """Module N queens
 """
-from sys import argv
+import sys
 
 
-if len(argv) >= 3:
-    print('Usage: nqueens N')
-    exit(1)
-
-try:
-    if int(argv[1]) < 4:
-        print('N must be at least 4')
-        exit(1)
-except ValueError:
-    print('N must be a number')
-
-
-def is_safe(board, row, col, N):
-    """function that check safe place to place Q
+def find_solutions(row, column):
+    """function that find solution
     """
-    for i in range(col):
-        if board[row][i] == 1:
-            return False
-
-    for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
-        if board[i][j] == 1:
-            return False
-
-    for i, j in zip(range(row, N, 1), range(col, -1, -1)):
-        if board[i][j] == 1:
-            return False
-
-    return True
+    solution = [[]]
+    for queen in range(row):
+        solution = N_palce_queen(queen, column, solution)
+    return solution
 
 
-def solve_n_queens(board, col, N):
-    """function that use recursion to check all possibility
+def N_palce_queen(queen, column, prev_solution):
+    """function that place n in place
     """
-    if col >= N:
-        return True
-
-    for i in range(N):
-        if is_safe(board, i, col, N):
-            board[i][col] = 1
-            if solve_n_queens(board, col + 1, N):
-                return True
-            board[i][col] = 0
-
-    return False
+    safe_position = []
+    for array in prev_solution:
+        for x in range(column):
+            if is_safe(queen, x, array):
+                safe_position.append(array + [x])
+    return safe_position
 
 
-def print_solution(board, N):
-    """function that print solution
+def is_safe(q, x, array):
+    """check save place of n
     """
-    new_list1 = []
-    new_list2 = []
-    count = 0
-    for i in range(N):
-        count = 0
-        for j in range(N):
-            if board[i][j] == 1:
-                new_list1.append([i, j])
-                new_list2.append([count, i])
-            count += 1
-    print(new_list2)
-    print(new_list1)
+    if x in array:
+        return (False)
+    else:
+        return all(abs(array[column] - x) != q - column
+                   for column in range(q))
 
 
-def n_queens(N):
-    """main function that start logic
+def n_queens():
+    """main function to start logic
     """
-    board = [[0] * N for _ in range(N)]
-    if not solve_n_queens(board, 0, N):
-        print("Solution does not exist")
-        return False
 
-    print_solution(board, N)
-    return True
+    if len(sys.argv) >= 3:
+        print('Usage: nqueens N')
+        sys.exit(1)
+
+    try:
+        if int(sys.argv[1]) < 4:
+            print('N must be at least 4')
+            sys.exit(1)
+    except ValueError:
+        print('N must be a number')
+        sys.exit(1)
+    n = int(sys.argv[1])
+    solutions = find_solutions(n, n)
+    for array in solutions:
+        new_list = []
+        for q, x in enumerate(array):
+            new_list.append([q, x])
+        print(new_list)
 
 
-n_queens(int(argv[1]))
+if __name__ == '__main__':
+    n_queens()
