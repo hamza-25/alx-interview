0f8538bba0fadcfca5cc606eb0495d3496429548
hamza-25 <me677499@gmail.com>
2024-04-13 21:22:18 +0000

    final version

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,126 +1,31 @@
 #!/usr/bin/python3
-# """Module N queens
-# """
-# from sys import argv
-
-
-# def is_safe(board, row, col, N):
-#     """function that check safe place to place Q
-#     """
-#     for i in range(col):
-#         if board[row][i] == 1:
-#             return False
-
-#     for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
-#         if board[i][j] == 1:
-#             return False
-
-#     for i, j in zip(range(row, N, 1), range(col, -1, -1)):
-#         if board[i][j] == 1:
-#             return False
-
-#     return True
-
-
-# def solve_n_queens(board, col, N):
-#     """function that use recursion to check all possibility
-#     """
-#     if col >= N:
-#         return True
-
-#     for i in range(N):
-#         if is_safe(board, i, col, N):
-#             board[i][col] = 1
-#             if solve_n_queens(board, col + 1, N):
-#                 return True
-#             board[i][col] = 0
-
-#     return False
-
-
-# def print_solution(board, N):
-#     """function that print solution
-#     """
-#     new_list1 = []
-#     new_list2 = []
-#     count = 0
-#     for i in range(N):
-#         count = 0
-#         for j in range(N):
-#             if board[i][j] == 1:
-#                 new_list1.append([i, j])
-#                 new_list2.append([count, i])
-#             count += 1
-#     print(new_list2)
-#     print(new_list1)
-
-
-# def n_queens(N):
-#     """main function that start logic
-#     """
-
-#     if len(argv) != 2:
-#         print('Usage: nqueens N')
-#         sys.exit(1)
-
-#     try:
-#         if int(argv[1]) < 4:
-#             print('N must be at least 4')
-#             sys.exit(1)
-#     except ValueError:
-#         print('N must be a number')
-#         sys.exit(1)
-
-#     board = [[0] * N for _ in range(N)]
-#     if not solve_n_queens(board, 0, N):
-#         print("Solution does not exist")
-#         return False
-
-#     print_solution(board, N)
-#     return True
-
-# if __name__ == '__main__':
-#     n_queens(int(argv[1]))
+"""Module N queens
+"""
 import sys
 
 
-def generate_solutions(row, column):
+def solutions_generate(row, col):
     solution = [[]]
     for queen in range(row):
-        solution = place_queen(queen, column, solution)
+        solution = place_n_queen(queen, col, solution)
     return solution
 
 
-def place_queen(queen, column, prev_solution):
+def place_n_queen(queen, col, previous_solution):
     safe_position = []
-    for array in prev_solution:
-        for x in range(column):
+    for array in previous_solution:
+        for x in range(col):
             if is_safe(queen, x, array):
                 safe_position.append(array + [x])
     return safe_position
 
 
-def is_safe(q, x, array):
-    if x in array:
+def is_safe(q, value, array):
+    if value in array:
         return (False)
     else:
-        return all(abs(array[column] - x) != q - column
-                   for column in range(q))
-
-
-# def init():
-#     if len(sys.argv) != 2:
-#         print("Usage: nqueens N")
-#         sys.exit(1)
-#     if sys.argv[1].isdigit():
-#         n = int(sys.argv[1])
-#     else:
-#         print("N must be a number")
-#         sys.exit(1)
-#     if n < 4:
-#         print("N must be at least 4")
-#         sys.exit(1)
-#     return (n)
+        return all(abs(array[col] - value) != q - col
+                   for col in range(q))
 
 
 def n_queens():
@@ -137,14 +42,14 @@ def n_queens():
         print('N must be a number')
         sys.exit(1)
     n = int(sys.argv[1])
-    # generate all solutions
-    solutions = generate_solutions(n, n)
-    # print solutions
+
+    solutions = solutions_generate(n, n)
+
     for array in solutions:
-        clean = []
-        for q, x in enumerate(array):
-            clean.append([q, x])
-        print(clean)
+        new_arr = []
+        for index, value in enumerate(array):
+            new_arr.append([index, value])
+        print(new_arr)
 
 
 if __name__ == '__main__':
