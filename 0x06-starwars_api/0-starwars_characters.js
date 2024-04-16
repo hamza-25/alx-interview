8eb15aaca9ad4e057a3080a986f227d75a0534a9
hamza-25 <me677499@gmail.com>
2024-04-16 23:52:14 +0100

    use request module

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,4 +1,5 @@
 #!/usr/bin/node
+const request = require('request');
 // const fetchApi = fetch("https://swapi-api.alx-tools.com/api/films/")
 // fetchApi.then((res) => {
 //     const jsonPromise = res.json();
@@ -19,26 +20,59 @@
 //     });
 
 // });
-const itemID = process.argv[2] - 1;
 
-const fetchApi = fetch('https://swapi-api.alx-tools.com/api/films/');
+// const itemID = process.argv[2] - 1;
 
-fetchApi.then((res) => {
-  const jsonPromise = res.json();
+// const fetchApi = fetch('https://swapi-api.alx-tools.com/api/films/');
 
-  jsonPromise.then((data) => {
-    const itemNeeded = data.results[itemID];
+// fetchApi.then((res) => {
+//   const jsonPromise = res.json();
+
+//   jsonPromise.then((data) => {
+//     const itemNeeded = data.results[itemID];
+//     const promises = [];
+
+//     itemNeeded.characters.forEach((element, index) => {
+//       const linkApi = fetch(element);
+//       promises.push(linkApi.then((response) => response.json()));
+//     });
+
+//     Promise.all(promises).then((characters) => {
+//       characters.forEach((data) => {
+//         console.log(`${data.name}`);
+//       });
+//     });
+//   });
+// });
+
+request('https://swapi-api.alx-tools.com/api/films/', (error, response, body) => {
+  if (!error && response.statusCode === 200) {
+    const data = JSON.parse(body);
+    const itemNeeded = data.results[2];
+    const characters = itemNeeded.characters;
     const promises = [];
 
-    itemNeeded.characters.forEach((element, index) => {
-      const linkApi = fetch(element);
-      promises.push(linkApi.then((response) => response.json()));
+    characters.forEach((element, index) => {
+      promises.push(new Promise((resolve, reject) => {
+        request(element, (error, response, body) => {
+          if (!error && response.statusCode === 200) {
+            const characterData = JSON.parse(body);
+            resolve({ index, name: characterData.name });
+          } else {
+            reject(error);
+          }
+        });
+      }));
     });
 
-    Promise.all(promises).then((characters) => {
-      characters.forEach((data) => {
-        console.log(`${data.name}`);
+    Promise.all(promises).then((results) => {
+      results.forEach((result) => {
+        console.log(`${result.index} - ${result.name}`);
       });
+    }).catch((error) => {
+      console.error('Error:', error);
     });
-  });
+  } else {
+    console.error('Error:', error);
+  }
 });
