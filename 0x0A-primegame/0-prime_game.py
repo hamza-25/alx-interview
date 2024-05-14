01d00616ea64a1e89024c305593eb56310a4e6bc
hamza-25 <me677499@gmail.com>
2024-05-14 17:11:15 +0100

    fix most turns win

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -30,46 +30,29 @@ def multiple_of_num(num):
 
 def isWinner(x, nums):
     """Determine the winner of the game."""
-    # round = 1
-    # is_maria_turn = False
-    # for num in nums:
-    #     if round > x:
-    #         break
-    #     if is_prime_number(num):
-    #         multiples = multiple_of_num(num)
-    #         for i in range(len(nums)):
-    #             if nums[i] in multiples:
-    #                 nums[i] = 0
-    #         if is_maria_turn:
-    #             return "Maria"
-    #         else:
-    #             return "Ben"
-    #     is_maria_turn = not is_maria_turn
-    #     round += 1
-    # return None
-    maria_wins = 0
-    ben_wins = 0
-    for i in range(x):
-        num = nums[i]
-        maria_turn = (i % 2 == 0)
-
-        # Find primes and mark their multiples
-        for j in range(2, num + 1):
-            if is_prime_number(j):
-                for k in range(j, num + 1, j):
-                    if nums[i] == 0:  # Skip marked multiples
-                        continue
-                    if k % j == 0:
-                        nums[i] = 0  # Mark as multiple
-
-        if maria_turn:
-            maria_wins += 1 if sum(nums) > 0 else 0
-        else:
-            ben_wins += 1 if sum(nums) > 0 else 0
-
-    if maria_wins > ben_wins:
-        return "Maria"
-    elif ben_wins > maria_wins:
-        return "Ben"
-    else:
+    round = 1
+    ben_win = 0
+    maria_win = 0
+    is_maria_turn = False
+    for num in nums:
+        if round > x:
+            break
+        if is_prime_number(num):
+            multiples = multiple_of_num(num)
+            for i in range(len(nums)):
+                if nums[i] in multiples:
+                    nums[i] = 0
+            if is_maria_turn:
+                # return "Maria"
+                maria_win += 1
+            else:
+                # return "Ben"
+                ben_win += 1
+        is_maria_turn = not is_maria_turn
+        round += 1
+    if maria_win == ben_win:
         return None
+    elif maria_win > ben_win:
+        return "Maria"
+    else:
+        return "Ben"
