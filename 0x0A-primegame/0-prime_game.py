f8e0f85e97095d4702579c3cfa4a10ac50b409c5
hamza-25 <me677499@gmail.com>
2024-05-14 16:47:07 +0100

    add task is winner

---
+++ 0x0A-primegame/0-prime_game.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+"""defined module for game prime number
+"""
+from typing import List
+
+
+def is_prime_number(num: int) -> bool:
+    """check the number is prime or not
+    Return boolean
+    """
+    if num == 0:
+        return False
+    for i in range(2, num):
+        if num % i == 0:
+            return False
+    return True
+
+
+def multiple_of_num(num: int, nums_list: List[int]) -> List[int]:
+    """Generate a list of multiples of num."""
+    multi_nums = []
+    i = 2
+    while True:
+        result = num * i
+        if result > 10000:
+            break
+        multi_nums.append(result)
+        i += 1
+    return multi_nums
+
+
+def isWinner(x: int, nums: List[int]) -> str:
+    """Determine the winner of the game."""
+    round = 1
+    is_maria_turn = False
+    for num in nums:
+        if round > x:
+            break
+        if is_prime_number(num):
+            multiples = multiple_of_num(num, nums)
+            for i in range(len(nums)):
+                if nums[i] in multiples:
+                    nums[i] = 0
+            if is_maria_turn:
+                return "Maria"
+            else:
+                return "Ben"
+        is_maria_turn = not is_maria_turn
+        round += 1
+    return None
