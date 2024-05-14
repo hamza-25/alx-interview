b8b52046a140da45e2f980951fc86a5b685038a3
hamza-25 <me677499@gmail.com>
2024-05-14 19:28:53 +0100

    test

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -60,35 +60,29 @@
 #     else:
 #         return "Ben"
 
-def is_prime_number(num):
-    """Check if a number is prime."""
-    if num < 2:
-        return False
-    for i in range(2, int(num**0.5) + 1):
-        if num % i == 0:
-            return False
-    return True
-
-
 def isWinner(x, nums):
-    """Determine the winner of the game."""
-    maria_win = 0
-    ben_win = 0
-
-    for n in nums:
-        if n == 1 or not is_prime_number(n):
-            continue  # Skip non-prime numbers
-
-        # Calculate the winner based on the starting player
-        if n % 2 == 0:  # Ben's turn
-            ben_win += 1
-        else:  # Maria's turn
-            maria_win += 1
-
-    # Determine the overall winner
-    if maria_win == ben_win:
+    """function that checks for the winner"""
+    if not nums or x < 1:
         return None
-    elif maria_win > ben_win:
+    max_num = max(nums)
+
+    my_filter = [True for _ in range(max(max_num + 1, 2))]
+    for i in range(2, int(pow(max_num, 0.5)) + 1):
+        if not my_filter[i]:
+            continue
+        for j in range(i * i, max_num + 1, i):
+            my_filter[j] = False
+    my_filter[0] = my_filter[1] = False
+    y = 0
+    for i in range(len(my_filter)):
+        if my_filter[i]:
+            y += 1
+        my_filter[i] = y
+    player1 = 0
+    for x in nums:
+        player1 += my_filter[x] % 2 == 1
+    if player1 * 2 == len(nums):
+        return None
+    if player1 * 2 > len(nums):
         return "Maria"
-    else:
-        return "Ben"
+    return "Ben"
